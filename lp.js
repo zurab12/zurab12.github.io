75bba86109699880c9a0e640f86a1df635a79dc5
zurab12 <zhenya.nikolaev.68@mail.ru>
2026-01-31 08:12:42 +0300

    Update lp.js

--- lp.js
+++ lp.js
@@ -7,7 +7,7 @@
       Lampa.Storage.set('lampac_unic_id', unic_id);
     }
 
-    Lampa.Storage.set('torrserver_url','owajzcze.deploy.cx/ts');
+    Lampa.Storage.set('torrserver_url','lam10.akter-black.com/ts');
     Lampa.Storage.set('torrserver_auth','true');
     Lampa.Storage.set('torrserver_login',Lampa.Storage.get('account_email') || Lampa.Storage.get('lampac_unic_id', '') || 'ts');
     Lampa.Storage.set('torrserver_password','ts');
